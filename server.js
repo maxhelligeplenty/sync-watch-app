42280b5ddc47acb4aeee56093f8b51c9dbe720e7
Max Hellige <max.hellige@plentymarkets.com>
2019-02-01 04:21:36 +0100

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -4,15 +4,15 @@ Object.defineProperty(exports, "__esModule", {value: true});
 const express = require('express');
 const path = require('path');
 
-// const app = express();
-//
+const app = express();
+
 // const video_sync_server_1 = require("./video-sync-server");
 // const server = new video_sync_server_1.VideoSyncServer().getApp();
 // exports.server = server;
 
-const video_sync_server_1 = require("./video-sync-server");
-const app = new video_sync_server_1.VideoSyncServer().getApp();
-exports.app = app;
+// const video_sync_server_1 = require("./video-sync-server");
+// const app = new video_sync_server_1.VideoSyncServer().getApp();
+// exports.app = app;
 
 
 // Serve only the static files form the dist directory
@@ -23,4 +23,4 @@ app.get('/*', function(req,res) {
     res.sendFile(path.join(__dirname+'/dist/sync-watch-application/index.html'));
 });
 
-// app.listen(process.env.PORT || 4200);
+app.listen(process.env.PORT || 4200);
