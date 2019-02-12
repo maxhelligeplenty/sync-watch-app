712f25c1647c85075cd60ce322774001d3179f61
Max Hellige <max.hellige@plentymarkets.com>
2019-02-12 05:49:42 +0100

    add docker file

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const socketIO = require('socket.io');
 const path = require('path');
 const http_1 = require("http");
-const event_interface_1 = require("../sync-watch-application/model/event.interface");
+// const event_interface_1 = require("/model/event.interface");
 
 const app = express();
 
@@ -17,69 +17,93 @@ const io = socketIO(server);
 
 server.listen(process.env.PORT || 8080);
 
-io.on(event_interface_1.Event.CONNECT, function (socket) {
+console.log('listen to port 8080');
+
+var Event;
+(function (Event) {
+    Event["CONNECT"] = "connect";
+    Event["DISCONNECT"] = "disconnect";
+    Event["SYNC_TIME"] = "sync-time";
+    Event["NEW_VIDEO"] = "new-video";
+    Event["ASK_VIDEO_INFORMATION"] = "ask-video-info";
+    Event["SYNC_VIDEO_INFORMATION"] = "sync-video-info";
+    Event["STATE"] = "state";
+    Event["PLAY"] = "play";
+    Event["PAUSE"] = "pause";
+    Event["JOIN"] = "join";
+    Event["SEND_MESSAGE"] = "send-message";
+    Event["PLAY_NEW_VIDEO"] = "play-new-video";
+    Event["SEND_MAIL"] = "send-mail";
+    Event["SYNC_CURRENT_ROOM_MEMBER"] = "sync-current-room-member";
+    Event["ALERT_MEMBERS_NEW_USER"] = "alert-members-new-user";
+    Event["GET_USER_ROLE"] = "get-user-role";
+    Event["ASK_VIDEO_TIME"] = "ask-video-time";
+    Event["SYNC_TIME_ON_JOIN"] = "sync-time-on-join";
+    Event["ASK_STATUS"] = "ask-status";
+    Event["SYNC_STATUS"] = "sync-status";
+})(Event = exports.Event || (exports.Event = {}));
+
+io.on(Event.CONNECT, function (socket) {
     console.log('connected');
     var room = '';
     var username = '';
-    socket.on(event_interface_1.Event.JOIN, function (r, c) {
+    socket.on(Event.JOIN, function (r, c) {
         socket.join(r);
         room = r;
         username = c;
-        io.to(room).emit(event_interface_1.Event.SEND_MESSAGE, {
+        io.to(room).emit(Event.SEND_MESSAGE, {
             content: username + ' connected'
         });
-        console.log('connected');
     });
-    socket.on(event_interface_1.Event.SEND_MESSAGE, function (m) {
-        io.to(room).emit(event_interface_1.Event.SEND_MESSAGE, m);
+    socket.on(Event.SEND_MESSAGE, function (m) {
+        io.to(room).emit(Event.SEND_MESSAGE, m);
     });
-    socket.on(event_interface_1.Event.DISCONNECT, function () {
-        io.to(room).emit(event_interface_1.Event.SEND_MESSAGE, {
+    socket.on(Event.DISCONNECT, function () {
+        io.to(room).emit(Event.SEND_MESSAGE, {
             content: username + ' disconnected'
         });
-        console.log('disconnected');
     });
-    socket.on(event_interface_1.Event.PLAY, function () {
-        socket.to(room).emit(event_interface_1.Event.PLAY);
+    socket.on(Event.PLAY, function () {
+        socket.to(room).emit(Event.PLAY);
     });
-    socket.on(event_interface_1.Event.PAUSE, function () {
-        socket.to(room).emit(event_interface_1.Event.PAUSE);
+    socket.on(Event.PAUSE, function () {
+        socket.to(room).emit(Event.PAUSE);
     });
-    socket.on(event_interface_1.Event.SYNC_TIME, function (t) {
-        socket.to(room).emit(event_interface_1.Event.SYNC_TIME, t);
+    socket.on(Event.SYNC_TIME, function (t) {
+        socket.to(room).emit(Event.SYNC_TIME, t);
     });
-    socket.on(event_interface_1.Event.NEW_VIDEO, function (i) {
-        io.to(room).emit(event_interface_1.Event.NEW_VIDEO, i);
+    socket.on(Event.NEW_VIDEO, function (i) {
+        io.to(room).emit(Event.NEW_VIDEO, i);
     });
-    socket.on(event_interface_1.Event.PLAY_NEW_VIDEO, function (id) {
-        io.to(room).emit(event_interface_1.Event.PLAY_NEW_VIDEO, id);
+    socket.on(Event.PLAY_NEW_VIDEO, function (id) {
+        io.to(room).emit(Event.PLAY_NEW_VIDEO, id);
     });
     // TODO emit to HOST user so just one client send info
-    socket.on(event_interface_1.Event.ASK_VIDEO_INFORMATION, function (socketId) {
-        io.to(room).emit(event_interface_1.Event.ASK_VIDEO_INFORMATION, socketId);
+    socket.on(Event.ASK_VIDEO_INFORMATION, function (socketId) {
+        io.to(room).emit(Event.ASK_VIDEO_INFORMATION, socketId);
     });
-    socket.on(event_interface_1.Event.SYNC_VIDEO_INFORMATION, function (v, socketId) {
-        socket.broadcast.to(socketId).emit(event_interface_1.Event.SYNC_VIDEO_INFORMATION, v);
+    socket.on(Event.SYNC_VIDEO_INFORMATION, function (v, socketId) {
+        socket.broadcast.to(socketId).emit(Event.SYNC_VIDEO_INFORMATION, v);
     });
-    socket.on(event_interface_1.Event.ALERT_MEMBERS_NEW_USER, function (u) {
-        io.to(room).emit(event_interface_1.Event.ALERT_MEMBERS_NEW_USER, u);
+    socket.on(Event.ALERT_MEMBERS_NEW_USER, function (u) {
+        io.to(room).emit(Event.ALERT_MEMBERS_NEW_USER, u);
     });
-    socket.on(event_interface_1.Event.SYNC_CURRENT_ROOM_MEMBER, function (u, socketId) {
-        socket.to(socketId).emit(event_interface_1.Event.SYNC_CURRENT_ROOM_MEMBER, u);
+    socket.on(Event.SYNC_CURRENT_ROOM_MEMBER, function (u, socketId) {
+        socket.to(socketId).emit(Event.SYNC_CURRENT_ROOM_MEMBER, u);
     });
-    socket.on(event_interface_1.Event.GET_USER_ROLE, function (u) {
-        io.to(room).emit(event_interface_1.Event.GET_USER_ROLE, u);
+    socket.on(Event.GET_USER_ROLE, function (u) {
+        io.to(room).emit(Event.GET_USER_ROLE, u);
     });
-    socket.on(event_interface_1.Event.ASK_VIDEO_TIME, function (socketId) {
-        io.to(room).emit(event_interface_1.Event.ASK_VIDEO_TIME, socketId);
+    socket.on(Event.ASK_VIDEO_TIME, function (socketId) {
+        io.to(room).emit(Event.ASK_VIDEO_TIME, socketId);
     });
-    socket.on(event_interface_1.Event.SYNC_TIME_ON_JOIN, function (socketId, t) {
-        socket.to(socketId).emit(event_interface_1.Event.SYNC_TIME_ON_JOIN, t);
+    socket.on(Event.SYNC_TIME_ON_JOIN, function (socketId, t) {
+        socket.to(socketId).emit(Event.SYNC_TIME_ON_JOIN, t);
     });
-    socket.on(event_interface_1.Event.ASK_STATUS, function (socketId) {
-        io.to(room).emit(event_interface_1.Event.ASK_STATUS, socketId);
+    socket.on(Event.ASK_STATUS, function (socketId) {
+        io.to(room).emit(Event.ASK_STATUS, socketId);
     });
-    socket.on(event_interface_1.Event.SYNC_STATUS, function (socketId, s) {
-        socket.to(socketId).emit(event_interface_1.Event.SYNC_STATUS, s);
+    socket.on(Event.SYNC_STATUS, function (socketId, s) {
+        socket.to(socketId).emit(Event.SYNC_STATUS, s);
     });
 });
